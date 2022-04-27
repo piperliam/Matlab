7bba64c3724c136069c0be36452263f86151efcb
Piperliam <68978670+piperliam@users.noreply.github.com>
2022-04-27 16:43:46 -0400

    Add files via upload

---
+++ Control systems/HW2.5/Publishing_Generic_Format_DotMFile.m
@@ -0,0 +1,46 @@
+%% Problem X
+
+clc; close all; clear all;
+% Perhaps initialize the problem here somewhere or start defining variables
+
+%% 
+% *Method:* You can include or start mentioning the method somewhere in
+% here. We can write equations in LaTeX typesetting such as in the example
+% that I will show here: $C_{L}=\frac{2L}{\rho V^{2}S}$. Pay particular
+% attention to the spaces and the method here. If you delete the "percent"
+% symbols in line 6, what happens to the published script? What happens if
+% you delete the space between the next line and the percent symbol? What
+% happens if you delete the asterisks in line 7?
+
+% Code implementation. You can replace this line with more code
+% implementation.
+
+%%
+% *Results:* You can, perhaps, include this right before including a plot
+% or a set of command window printing. I will include a random example just
+% to get some idea across.
+
+x=0:1:10;
+y=2*x;
+plot(x,y)
+grid on;
+title('Random Linear Plot')
+xlabel('x_{rand}')
+ylabel('y_{rand}')
+
+%%
+% *Discussion:* This is simply providing some skeleton that you may be able
+% to work with on future assignments. This allows you to create some markup
+% without having to create or compile images together. If you want to
+% publish a single pdf file, you may have to be careful in creating
+% variables and running the script, but it is possible to include more than
+% 1 problem here.
+
+%% Problem X2
+
+% Redefine or initialize values for this problem
+
+%%
+% *Method* You can basically reapeat what was done above for the Results
+% and Discussion all in the same file if you want to minimize the number of
+% files that are merged together.
